56c5b20b4d2811b886b8c6983f700fde284663a8
raulntjj <rauloliveira5230@gmail.com>
2025-06-07 18:35:25 -0300

    FEAT: finalizing the development of the environment

--- payment-service/index.js
+++ payment-service/index.js
@@ -4,7 +4,7 @@ fastify.get('/ping', async () => {
   return { status: 'ok', message: 'pong' };
 });
 
-fastify.listen({ port: 3000 }, (err) => {
+fastify.listen({ port: 3000, host: '0.0.0.0' }, (err) => {
   if (err) {
     console.error(err);
     process.exit(1);
