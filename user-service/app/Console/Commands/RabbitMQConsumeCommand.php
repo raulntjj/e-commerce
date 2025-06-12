7a369406adf5b0cea5d588105461f3593a90fa4c
raulntjj <rauloliveira5230@gmail.com>
2025-06-11 23:20:45 -0300

    FEAT: Implementing messaging service with RabbitMQ in user-service

---
+++ user-service/app/Console/Commands/RabbitMQConsumeCommand.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Listeners\AuditUserActivityListener;
+use App\Services\RabbitMQService;
+use Illuminate\Console\Command;
+use PhpAmqpLib\Message\AMQPMessage;
+
+class RabbitMQConsumeCommand extends Command {
+    protected $signature = 'rabbitmq:consume';
+    protected $description = 'Consume messages from RabbitMQ';
+    private RabbitMQService $rabbitMQService;
+
+    public function __construct(RabbitMQService $rabbitMQService) {
+        parent::__construct();
+        $this->rabbitMQService = $rabbitMQService;
+    }
+
+    public function handle(): void {
+        $queueName = 'audit_queue';
+        $routingKey = 'user.*'; 
+
+        $listener = new AuditUserActivityListener();
+
+        $this->info(" [*] Audit worker is now listening for routing key '$routingKey'. To exit press CTRL+C");
+
+        $this->rabbitMQService->consume($queueName, $routingKey, function (AMQPMessage $msg) use ($listener): void {
+            
+            $this->line("\n> Event Received: [" . $msg->getRoutingKey() . "]");
+            
+            $listener->handle($msg);
+            
+            $this->info("  - Event successfully audited.");
+        });
+    }
+}
--- user-service/app/Services/RabbitMQService.php
+++ user-service/app/Services/RabbitMQService.php
@@ -10,21 +10,38 @@ class RabbitMQService {
     protected $channel;
 
     public function __construct() {
-        $host = env('RABBITMQ_HOST', 'rabbitmq');
-        $port = 5672;
-        $user = env('RABBITMQ_DEFAULT_USER', 'root');
-        $pass = env('RABBITMQ_DEFAULT_PASS', 'root');
-        
+        $host = config('services.rabbitmq.host');
+        $port = config('services.rabbitmq.port');
+        $user = config('services.rabbitmq.user');
+        $pass = config('services.rabbitmq.pass');
         $this->connection = new AMQPStreamConnection($host, $port, $user, $pass);
         $this->channel = $this->connection->channel();
     }
 
-    public function publish($routingKey, $message) {
+    public function publish($routingKey, $message): void {
         $exchange = 'ecommerce_events';
         $this->channel->exchange_declare($exchange, 'topic', false, true, false);
 
         $msg = new AMQPMessage(json_encode($message), ['delivery_mode' => AMQPMessage::DELIVERY_MODE_PERSISTENT]);
         $this->channel->basic_publish($msg, $exchange, $routingKey);
+    }
+
+    public function consume(string $queueName, string $routingKey, \Closure $callback): void {
+        $exchange = 'ecommerce_events';
+        $this->channel->exchange_declare($exchange, 'topic', false, true, false);
+        $this->channel->queue_declare($queueName, false, true, false, false);
+        $this->channel->queue_bind($queueName, $exchange, $routingKey);
+
+        echo " [*] Waiting for messages for routing key '$routingKey'. To exit press CTRL+C\n";
+
+        $this->channel->basic_consume($queueName, '', false, false, false, false, function (AMQPMessage $msg) use ($callback) {
+            $callback($msg);
+            $msg->ack();
+        });
+
+        while ($this->channel->is_consuming()) {
+            $this->channel->wait();
+        }
     }
 
     public function __destruct() {
